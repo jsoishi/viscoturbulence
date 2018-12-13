7c6e7a783257fb79d1574becd19a0f1078547b11
J. S. Oishi <jsoishi@gmail.com>
2018-12-13 16:37:42 -0500

    Working version...

--- viscoturb.py
+++ viscoturb.py
@@ -38,21 +38,24 @@ logger.info("Using config file {}".format(config_file))
 
 # parameters
 params = runconfig['params']
+nL = params.getint('nL')
 nx = params.getint('nx')
 ny = params.getint('ny')
 Re = params.getfloat('Re')
 Wi = params.getfloat('Wi')
 eta = params.getfloat('eta')
 
-# Always on 2pi domain
-x = de.Fourier('x',nx)
-y = de.Fourier('y',ny)
+# always on square domain
+L = nL * 2 * np.pi
+x = de.Fourier('x',nx, interval=[0, L])
+y = de.Fourier('y',ny, interval=[0, L])
 
 domain = de.Domain([x,y], grid_dtype='float', mesh=mesh)
 
 variables = ['u', 'v', 'p',  'lU11', 'U12', 'lU22']
 
 problem = de.IVP(domain, variables=variables)
+problem.parameters['L'] = L
 problem.parameters['η'] = eta
 problem.parameters['Re'] = Re
 problem.parameters['Wi'] = Wi
@@ -66,12 +69,14 @@ problem.substitutions['Div_σ_x'] = "dx(σ11) + dy(σ12)"
 problem.substitutions['Div_σ_y'] = "dx(σ12) + dy(σ22)"
 
 # Navier-Stokes
-problem.add_equation("dt(u) - Lap(u)/(Re*(1+η)) + dx(p) = 2*η*Div_σ_x/(Wi*Re*(1+η)) - u*dx(u) - v*dy(u) - cos(y)/Re")
+problem.add_equation("dt(u) - Lap(u)/(Re*(1+η)) + dx(p) = 2*η*Div_σ_x/(Wi*Re*(1+η)) - u*dx(u) - v*dy(u) + cos(y)/Re")
 problem.add_equation("dt(v) - Lap(u)/(Re*(1+η)) + dy(p) = 2*η*Div_σ_y/(Wi*Re*(1+η)) - u*dx(v) - v*dy(v)")
+#problem.add_equation("dt(u) - Lap(u)/(Re*(1+η)) + dx(p) = -u*dx(u) - v*dy(u) - cos(y)/Re")
+#problem.add_equation("dt(v) - Lap(u)/(Re*(1+η)) + dy(p) = -u*dx(v) - v*dy(v)")
 
 #incompressibility
-problem.add_equation("dx(u) + dy(v) = 0", "nx != 0 or ny != 0")
-problem.add_equation("p = 0", "nx == 0 and ny == 0")
+problem.add_equation("dx(u) + dy(v) = 0", condition="(nx != 0) or (ny != 0)")
+problem.add_equation("p = 0", condition="(nx == 0) and (ny == 0)")
 
 # conformation tensor evolution
 # use Cholsky Decomposition
@@ -84,6 +89,8 @@ solver = problem.build_solver(de.timesteppers.MCNAB2)
 logger.info('Solver built')
 
 run_opts = runconfig['run']
+dt = run_opts.getfloat('dt')
+
 if run_opts.getfloat('stop_wall_time'):
     solver.stop_wall_time = run_opts.getfloat('stop_wall_time')
 else:
@@ -99,33 +106,58 @@ if run_opts.getfloat('stop_sim_time'):
 else:
     solver.stop_sim_time = np.inf
 
+
+basedir = pathlib.Path('scratch')
+outdir = config_file.stem
+data_dir = basedir/outdir
+if domain.dist.comm.rank == 0:
+    if not data_dir.exists():
+        data_dir.mkdir()
+
 # Analysis
 analysis_tasks = []
-check = solver.evaluator.add_file_handler(os.path.join(data_dir,'checkpoints'), wall_dt=3540, max_writes=50)
+check = solver.evaluator.add_file_handler(data_dir/'checkpoints', wall_dt=3540, max_writes=50)
 check.add_system(solver.state)
 analysis_tasks.append(check)
 
-snap = solver.evaluator.add_file_handler(os.path.join(data_dir,'snapshots'), sim_dt=1e-2, max_writes=200)
+snap = solver.evaluator.add_file_handler(data_dir/'snapshots', sim_dt=1e-1, max_writes=200)
 snap.add_task("dx(v) - dy(u)", name='vorticity')
 snap.add_task("u")
 snap.add_task("v")
 snap.add_task("σ11")
 snap.add_task("σ12")
 snap.add_task("σ22")
-snap.add_task("σ11", name='σ11_kspace', layout='c')
-snap.add_task("σ12", name='σ12_kspace', layout='c')
-snap.add_task("σ22", name='σ22_kspace', layout='c')
+# snap.add_task("σ11", name='σ11_kspace', layout='c')
+# snap.add_task("σ12", name='σ12_kspace', layout='c')
+# snap.add_task("σ22", name='σ22_kspace', layout='c')
 analysis_tasks.append(snap)
 
-timeseries = solver.evaluator.add_file_handler(os.path.join(data_dir,'timeseries'), iter=100)
-timeseries.add_task("0.5*integ(u**2 + v**2)", name='Ekin')
-timeseries.add_task("integ(σ11 + σ22)", name='Σ')
+timeseries = solver.evaluator.add_file_handler(data_dir/'timeseries', iter=100)
+timeseries.add_task("0.5*integ(u**2 + v**2)/L**2", name='Ekin')
+timeseries.add_task("integ(σ11 + σ22)/L**2", name='Σ')
 
-dt = 1e-3
+# initial conditions
+xx, yy = domain.grids()
+u = solver.state['u']
+lU11 = solver.state['lU11']
+U12 = solver.state['U12']
+lU22 = solver.state['lU22']
+u['g'] = np.cos(yy)
+
+lU11['g'] = np.log(np.sqrt(1 + Wi**2/2 * np.sin(yy)**2))
+U12['g'] = -Wi/2 * np.sin(yy)/np.exp(lU11['g'])
+lU22['g'] = 0. # log(1)
+
+flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
+flow.add_property("0.5*integ(u**2 + v**2)", name="ekin")
+flow.add_property("integ(σ11 + σ22)", name="sigma")
+
 start  = time.time()
 while solver.ok:
     if (solver.iteration-1) % 10 == 0:
         logger.info("Step {:d}; Time = {:e}".format(solver.iteration, solver.sim_time))
+        logger.info("Total Ekin = {:10.7e}".format(flow.max("ekin")))
+        logger.info("Total Sigma = {:10.7e}".format(flow.max("sigma")))
     solver.step(dt)
 stop = time.time()
 
